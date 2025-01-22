6e3a22b812bceba9e42015de8981914b647b556e
johan <juanmerkahorro@gmail.com>
2025-01-22 17:14:57 -0500

    yea

--- routes/formularioRoutes.js
+++ routes/formularioRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { enviarFormulario } from '../controllers/formularioController.js'; // Cambiado
+import { enviarCorreo } from '../controllers/formularioController.js'; // Asegúrate de que el nombre coincida
 import { upload } from '../services/multerConfig.js';
 
 const router = express.Router();
 
 // Ruta para guardar datos y enviar correo
-router.post('/enviar-email', upload.single('pdf'), enviarFormulario); // Cambiado
+router.post('/enviar-email', upload.single('pdf'), enviarCorreo); // Usa el nombre correcto
 
 export default router;
