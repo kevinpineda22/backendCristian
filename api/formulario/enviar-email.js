e6cf3386d1b14cad62524f092da1eca3cc66ef97
Juan Manuel <juanmerkahorro@gmail.com>
2025-01-22 16:00:16 -0500

    vamo a ver

--- api/formulario/enviar-email.js
+++ api/formulario/enviar-email.js
@@ -1,6 +1,5 @@
-// /api/formulario/enviar-email.js
-
-import { enviarCorreo } from '../../controllers/formularioController.js';  // Importa la lógica del controlador
+// api/formulario/enviar-email.js
+import { enviarCorreo } from '../../controllers/formularioController.js';  // Importa el controlador
 import { upload } from '../../services/multerConfig.js';  // Importa la configuración de Multer
 
 export const config = {
@@ -10,6 +9,16 @@ export const config = {
 };
 
 export default async function handler(req, res) {
+  // Configuración de CORS
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Permite acceso desde cualquier dominio
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Métodos permitidos
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Cabeceras permitidas
+
+  // Si el método es OPTIONS, responde con 200 sin procesar la solicitud
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   if (req.method === 'POST') {
     // Multer maneja la carga de archivos
     upload(req, res, (err) => {
