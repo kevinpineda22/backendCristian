5ebdff017d6b6adb23b012d5ea3fa6cc6998f322
Juan Manuel <juanmerkahorro@gmail.com>
2025-01-23 14:59:24 -0500

    yaaa

--- routes/historialRoutes.js
+++ routes/historialRoutes.js
@@ -1,10 +1,8 @@
 import express from 'express';
-import { historial, getAllHistorial, updateHistorialStatus } from '../controllers/historialController.js';
+import { historial } from '../controllers/historialController.js';
 
 const router = express.Router();
 
 router.get('/historial/:correo', historial);
-router.get('/historial', getAllHistorial);
-router.put('/historial', updateHistorialStatus);
 
 export default router;
--- services/supabaseService.js
+++ services/supabaseService.js
@@ -1,40 +1,8 @@
-import { createClient } from '@supabase/supabase-js';
-import dotenv from 'dotenv';
+    import { createClient } from '@supabase/supabase-js';
+    import dotenv from 'dotenv';
 
-dotenv.config();
+    dotenv.config();
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
+    const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
-const uploadFile = async (file) => {
-  const { data, error } = await supabase.storage.from('your-bucket').upload(`public/${file.name}`, file);
-  return { data, error };
-};
-
-const getPublicUrl = (path) => {
-  const { publicURL, error } = supabase.storage.from('your-bucket').getPublicUrl(path);
-  return { publicURL, error };
-};
-
-const insertRecord = async (record) => {
-  const { data, error } = await supabase.from('Automatizacion_cristian').insert([record]);
-  return { data, error };
-};
-
-const getRecordsByEmail = async (email) => {
-  const { data, error } = await supabase.from('Automatizacion_cristian').select('*').eq('correo_asignado', email);
-  return { data, error };
-};
-
-const getAllRecords = async () => {
-  const { data, error } = await supabase.from('Automatizacion_cristian').select('*');
-  return { data, error };
-};
-
-const updateRecordStatusAndObservation = async (id, estado, observacion) => {
-  const { data, error } = await supabase.from('Automatizacion_cristian').update({ estado, observacion }).eq('id', id);
-  return { data, error };
-};
-
-export { uploadFile, getPublicUrl, insertRecord, getRecordsByEmail, getAllRecords, updateRecordStatusAndObservation };
+    export default supabase;
