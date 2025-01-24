7e24082e7944f5a173c2789059cc0d915a90d8f9
johan <juanmerkahorro@gmail.com>
2025-01-24 11:31:56 -0500

    pollos hermanos 6

--- controllers/registroController.js
+++ controllers/registroController.js
@@ -40,14 +40,32 @@ const registro = async (req, res) => {
     });
 
     if (error) {
-      console.error('Error al insertar el registro:', error.message);
-      return res.status(500).json({ error: 'Error al insertar el registro', details: error.message });
+      console.error('Error al insertar el registro:', {
+        message: error.message,
+        code: error.code,
+        details: JSON.stringify(error),
+        hint: error.hint,
+        query: error.query
+      });
+      return res.status(500).json({ error: 'Error al guardar registro', details: error.message });
     }
 
-    res.status(200).json({ message: 'Registro creado exitosamente', data });
+    // Enviar el correo electrónico
+    await sendEmail({
+      to: correo_asignado,
+      subject: 'Nuevo Proceso',
+      descripcion,
+      sede,
+      fecha_inicial,
+      fecha_final,
+      correo_asignado,
+      file
+    });
+
+    res.status(200).json({ message: 'Registro exitoso' });
   } catch (error) {
-    console.error('Error inesperado:', error.message);
-    res.status(500).json({ error: 'Error inesperado', details: error.message });
+    console.error('Error completo:', error);
+    res.status(500).json({ error: 'Error en registro', details: error.message });
   }
 };
 
